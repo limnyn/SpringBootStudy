43f417bc064696786f4f0e9b93fe042bddc346a5
limnyn <hogandi001@gmail.com>
2024-06-25 00:53:20 +0900

    first commit

---
+++ src/main/java/com/naver/shopping/SupperShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.naver.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupperShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupperShoppingApplication.class, args);
+	}
+
+}
