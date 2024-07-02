ebde5a943fc88573aa00855ffc78afc74eaaf90b
SSAFY_GallaxybookUltra3 <hogandi001@gmail.com>
2024-07-02 22:30:42 +0900

    feat : add bean

--- src/test/java/com/naver/shopping/member/MemberServiceTest.java
+++ src/test/java/com/naver/shopping/member/MemberServiceTest.java
@@ -4,11 +4,14 @@ package com.naver.shopping.member;
 import com.naver.shopping.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberServiceTest {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-
+//    AppConfig appConfig = new AppConfig();
+//    MemberService memberService = appConfig.memberService();
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = ac.getBean("memberService", MemberService.class);
     @Test //테스트 코드 Annotation
     void 회원가입() {
         Member member = new Member(1L, "홍길동", Grade.VIP);
