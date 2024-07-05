8ffd97bb5e2c2c8083f7bdfeb3ee4f4969218cd0
SSAFY_GallaxybookUltra3 <hogandi001@gmail.com>
2024-07-05 10:59:42 +0900

    feat : fix autoScanTest -> unused import delete

--- src/test/java/com/naver/shopping/autoScan/autoScanTest.java
+++ src/test/java/com/naver/shopping/autoScan/autoScanTest.java
@@ -13,7 +13,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
